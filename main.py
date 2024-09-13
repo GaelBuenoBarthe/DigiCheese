03341245e7859c84171981aa07dbafcb7d863745
GaelBuenoBarthe <gaelbueno@aol.com>
2024-09-13 11:00:53 +0200

    add models, database, crud

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from . import models, crud
+from .database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 def read_root():
     return {"Bienvenue dans Digicheese !"}
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+def read_item(item_id: int, db: Session = Depends(get_db)):
+    item = crud.get_item(db, item_id)
+    if item is None:
+        raise HTTPException(status_code=404, detail="Et non !!! c'est raté")
+    return item
