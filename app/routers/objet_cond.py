fd2a0e71b464086c156ec33cbd4aad26f54b4016
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2024-09-14 00:31:55 +0200

    [Task] Objet_cond APi inplementation

---
+++ app/routers/objet_cond.py
@@ -0,0 +1,37 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.schemas.objet_cond import ObjetCondCreate, ObjetCondResponse
+from app.infrastructure.api.stock.objet_condcontroller import (
+    get_all_objet_cond,
+    get_objet_cond,
+    create_objet_cond,
+    update_objet_cond,
+    delete_objet_cond,
+)
+
+router = APIRouter()
+
+# Get all ObjetCond entries
+@router.get("/", response_model=list[ObjetCondResponse])
+def route_get_all_objet_cond(db: Session, skip: int = 0, limit: int = 10 ):
+    return get_all_objet_cond(skip, limit, db)
+
+# Get an ObjetCond by ID
+@router.get("/{idrelcond}", response_model=ObjetCondResponse)
+def route_get_objet_cond(idrelcond: int, db: Session):
+    return get_objet_cond(idrelcond, db)
+
+# Create a new ObjetCond
+@router.post("/", response_model=ObjetCondResponse)
+def route_create_objet_cond(objet_cond: ObjetCondCreate, db: Session):
+    return create_objet_cond(objet_cond, db)
+
+# Update an existing ObjetCond
+@router.put("/{idrelcond}", response_model=ObjetCondResponse)
+def route_update_objet_cond(idrelcond: int, objet_cond_update: ObjetCondCreate, db: Session):
+    return update_objet_cond(idrelcond, objet_cond_update, db)
+
+# Delete an ObjetCond
+@router.delete("/{idrelcond}", response_model=ObjetCondResponse)
+def route_delete_objet_cond(idrelcond: int, db: Session):
+    return delete_objet_cond(idrelcond, db)
