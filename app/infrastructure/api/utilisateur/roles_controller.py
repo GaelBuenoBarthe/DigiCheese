904ced73461c46d2f4a39fd168952dc16a890218
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2024-09-14 12:12:41 +0200

    [Fix] Controller Definition

--- app/infrastructure/api/utilisateur/roles_controller.py
+++ app/infrastructure/api/utilisateur/roles_controller.py
@@ -1,32 +1,33 @@
-from fastapi import  HTTPException
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
 from app.models.utilisateur import role
 from app.schemas.role import RoleCreate, RoleResponse
 
 
+# Get all roles
+def get_roles(db: Session, skip: int = 0, limit: int = 10):
+    return db.query(role).offset(skip).limit(limit).all()
 
-@router.get("/", response_model=list[RoleResponse])
-def get_roles(db: Session, skip: int = 0, limit: int = 10 ):
-    roles = db.query(role).offset(skip).limit(limit).all()
-    return roles
 
-@router.post("/", response_model=RoleResponse)
-def create_role(role_data: RoleCreate, db: Session):
+# Create a new role
+def create_role(db: Session, role_data: RoleCreate):
     db_role = role(**role_data.dict())
     db.add(db_role)
     db.commit()
     db.refresh(db_role)
     return db_role
 
-@router.get("/{role_id}", response_model=RoleResponse)
-def get_role(role_id: int, db: Session):
+
+# Get a role by ID
+def get_role(db: Session, role_id: int):
     db_role = db.query(role).filter(role.id == role_id).first()
     if not db_role:
         raise HTTPException(status_code=404, detail="Role not found")
     return db_role
 
-@router.delete("/{role_id}", response_model=RoleResponse)
-def delete_role(role_id: int, db: Session):
+
+# Delete a role by ID
+def delete_role(db: Session, role_id: int):
     db_role = db.query(role).filter(role.id == role_id).first()
     if not db_role:
         raise HTTPException(status_code=404, detail="Role not found")
