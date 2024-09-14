8ac20a2767e59113d72ef4408af46e1ce5c90082
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2024-09-14 21:53:02 +0200

    [Patch] Update dapabase.py

--- app/database.py
+++ app/database.py
@@ -5,24 +5,29 @@ from sqlalchemy.ext.declarative import declarative_base
 # connexion a la base de donnée et déclaration de la base avec sql alchemy
 
 #Importation des modèles
-from app.models.commande.Commande import Commande
-from app.models.commande.Detail import Detail
-from app.models.commande.Detail_Objet import DetailObjet
-from app.models.utilisateur.Utilisateur import Utilisateur
-from app.models.utilisateur.Role import Role
-from app.models.stock.Objet_Cond import ObjetCond
-from app.models.stock.Poids import Poids
-from app.models.stock.Vignette import Vignette
-from app.models.stock.conditionnement import Conditionnement
-from app.models.stock.Objet import Objet
-from app.models.fidelite.ProgrammeFidelite import ProgrammeFidelite
-from app.models.fidelite.Promo import Promo
-from app.models.fidelite.Bonus import Bonus
-from app.models.fidelite.Transaction import Transaction
-from app.models.client.Departement import Departement
-from app.models.client.Commune import Commune
-from app.models.client.Client import Client
-from app.models.client.Enseigne import Enseigne
+
+def importmodels() :
+    from app.models.commande.Commande import Commande
+    from app.models.commande.Detail import Detail
+    from app.models.commande.Detail_Objet import DetailObjet
+    from app.models.utilisateur.Utilisateur import Utilisateur
+    from app.models.utilisateur.Role import Role
+    from app.models.stock.Objet_Cond import ObjetCond
+    from app.models.stock.Poids import Poids
+    from app.models.stock.Vignette import Vignette
+    from app.models.stock.conditionnement import Conditionnement
+    from app.models.stock.Objet import Objet
+    from app.models.fidelite.ProgrammeFidelite import ProgrammeFidelite
+    from app.models.fidelite.Promo import Promo
+    from app.models.fidelite.Bonus import Bonus
+    from app.models.fidelite.Transaction import Transaction
+    from app.models.client.Departement import Departement
+    from app.models.client.Commune import Commune
+    from app.models.client.Client import Client
+    from app.models.client.Enseigne import Enseigne
+
+
+importmodels()
 
 # url de connexion de la base
 SQLALCHEMY_DATABASE_URL = "mysql://dev:12345@localhost/fromagerie_com"
