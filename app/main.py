4eb0a15b339d2cd27504cfc21aed0e793efad018
GaelBuenoBarthe <gaelbueno@aol.com>
2024-09-16 12:32:00 +0200

    [patch] add css & landpage

--- app/main.py
+++ app/main.py
@@ -1,4 +1,9 @@
 from fastapi import FastAPI
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi.requests import Request
+from fastapi.openapi.docs import get_swagger_ui_html
+from starlette.staticfiles import StaticFiles
 
 from app.routers.utilisateur import roles_router, utilisateurs_router
 from app.routers.fidelite import programme_fidelite_router
@@ -8,6 +13,10 @@ from app.routers.client import clients_router, commune_router, departement_route
 from app.database import init_db
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
 
 app.include_router(commande_router.router, prefix="/commandes", tags=["commandes"])
 app.include_router(detail_router.router, prefix="/details", tags=["details"])
@@ -23,7 +32,24 @@ app.include_router(commune_router.router, prefix="/commune", tags=["Communes"])
 app.include_router(departement_router.router, prefix="/department", tags=["Departments"])
 app.include_router(enseigne_router.router, prefix="/enseigne", tags=["Enseignes"])
 app.include_router(conditionnement_router.router, prefix="/conditionnement", tags=["Conditionnements"])
-@app.get("/")
-def read_root():
-    return {"Bienvenue dans Digicheese !"}
+
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - Documentation",
+        swagger_favicon_url="/static/favicon.ico",
+        swagger_ui_parameters={"defaultModelsExpandDepth": -1},
+        swagger_css_url="/static/custom_swagger.css"
+    ) + """
+    <div style="text-align: center; margin-top: 20px;">
+        <a href="/" class="custom-link">Retour à l'index</a>
+        <a href="/another-link" class="custom-link">Un autre lien</a>
+    </div>
+    """
+
 init_db()
