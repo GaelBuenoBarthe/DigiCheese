488dc3ea8cb5754bf4b5ac0b91ae201375599d41
GaelBuenoBarthe <gaelbueno@aol.com>
2024-09-17 16:10:42 +0200

    [fixed] api response for utilisateur

--- app/infrastructure/api/utilisateur/utilisateurs_controller.py
+++ app/infrastructure/api/utilisateur/utilisateurs_controller.py
@@ -1,15 +1,22 @@
 from sqlalchemy.orm import Session
 from fastapi import HTTPException
 
-from app.models.utilisateur import utilisateur, role_utilisateur, role
+from app.models.utilisateur.role import Role
+from app.models.utilisateur.role_utilisateur import RoleUtilisateur
 from app.models.utilisateur.utilisateur import Utilisateur
 from app.schemas.utilisateur import UtilisateurCreate, UtilisateurUpdate, UtilisateurResponse
 
-
 # Get users from the database
 def get_users(db: Session, skip: int = 0, limit: int = 10):
-    return db.query(Utilisateur).offset(skip).limit(limit).all()
-
+    utilisateurs = db.query(Utilisateur).offset(skip).limit(limit).all()
+    return [
+        UtilisateurResponse(
+            id=user.code_utilisateur,
+            name=user.nom_utilisateur,
+            email=user.username
+        )
+        for user in utilisateurs
+    ]
 
 # Create a new user in the database
 def create_user(db: Session, user: UtilisateurCreate):
@@ -17,16 +24,22 @@ def create_user(db: Session, user: UtilisateurCreate):
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
-    return db_user
-
+    return UtilisateurResponse(
+        id=db_user.code_utilisateur,
+        name=db_user.nom_utilisateur,
+        email=db_user.username
+    )
 
 # Get a specific user by ID
 def get_user(db: Session, user_id: int):
     user = db.query(Utilisateur).filter(Utilisateur.code_utilisateur == user_id).first()
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
-    return user
-
+    return UtilisateurResponse(
+        id=user.code_utilisateur,
+        name=user.nom_utilisateur,
+        email=user.username
+    )
 
 # Update user information
 def update_user(db: Session, user_id: int, user_update: UtilisateurUpdate):
@@ -40,8 +53,11 @@ def update_user(db: Session, user_id: int, user_update: UtilisateurUpdate):
 
     db.commit()
     db.refresh(user)
-    return user
-
+    return UtilisateurResponse(
+        id=user.code_utilisateur,
+        name=user.nom_utilisateur,
+        email=user.username
+    )
 
 # Delete a user from the database
 def delete_user(db: Session, user_id: int):
@@ -51,8 +67,11 @@ def delete_user(db: Session, user_id: int):
 
     db.delete(user)
     db.commit()
-    return user
-
+    return UtilisateurResponse(
+        id=user.code_utilisateur,
+        name=user.nom_utilisateur,
+        email=user.username
+    )
 
 # Assign a role to a user
 def assign_role_to_user(db: Session, user_id: int, role_id: int):
@@ -64,17 +83,20 @@ def assign_role_to_user(db: Session, user_id: int, role_id: int):
     if not role_to_assign:
         raise HTTPException(status_code=404, detail="Role not found")
 
-    user_role = Role_Utilisateur(code_utilisateur=user_id, code_role=role_id)
+    user_role = RoleUtilisateur(code_utilisateur=user_id, code_role=role_id)
     db.add(user_role)
     db.commit()
-    return user
-
+    return UtilisateurResponse(
+        id=user.code_utilisateur,
+        name=user.nom_utilisateur,
+        email=user.username
+    )
 
 # Remove a role from a user
 def remove_role_from_user(db: Session, user_id: int, role_id: int):
-    user_role = db.query(Role_Utilisateur).filter(
-        Role_Utilisateur.code_utilisateur == user_id,
-        Role_Utilisateur.code_role == role_id
+    user_role = db.query(RoleUtilisateur).filter(
+        RoleUtilisateur.code_utilisateur == user_id,
+        RoleUtilisateur.code_role == role_id
     ).first()
 
     if not user_role:
@@ -82,4 +104,8 @@ def remove_role_from_user(db: Session, user_id: int, role_id: int):
 
     db.delete(user_role)
     db.commit()
-    return user_role
+    return UtilisateurResponse(
+        id=user_role.code_utilisateur,
+        name=user_role.utilisateur.nom_utilisateur,
+        email=user_role.utilisateur.username
+    )
