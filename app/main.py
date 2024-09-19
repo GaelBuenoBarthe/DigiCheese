9c7e01e0549d30b2e79a62bff53c2b052c9ea935
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2024-09-19 05:09:36 +0200

    [Fix] Organisation des endpoints en Main.py

--- app/main.py
+++ app/main.py
@@ -21,20 +21,29 @@ app.mount("/static", StaticFiles(directory=static_dir), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
-app.include_router(commande_router.router, prefix="/commandes", tags=["commandes"])
-app.include_router(detail_router.router, prefix="/details", tags=["details"])
-app.include_router(detail_objet_router.router, prefix="/detail_objets", tags=["DetailObjets"])
-app.include_router(utilisateurs_router.router, prefix="/utilisateurs", tags=["Utilisateurs"])
-app.include_router(roles_router.router, prefix="/roles", tags=["Roles"])
-app.include_router(vignette_router.router, prefix="/vignettes", tags=["Vignettes"])
-app.include_router(poids_router.router, prefix="/poids", tags=["Poids"])
-app.include_router(objet_cond_router.router, prefix="/objet-cond", tags=["ObjetCond"])
-app.include_router(programme_fidelite_router.router, prefix="/fidelite", tags=["Fidelite"])
+#Client Endpoints
 app.include_router(clients_router.router, prefix="/client", tags=["Clients"])
 app.include_router(commune_router.router, prefix="/commune", tags=["Communes"])
 app.include_router(departement_router.router, prefix="/department", tags=["Departments"])
 app.include_router(enseigne_router.router, prefix="/enseigne", tags=["Enseignes"])
+#commande Endpoints
+app.include_router(commande_router.router, prefix="/commandes", tags=["commandes"])
+app.include_router(detail_router.router, prefix="/details", tags=["details"])
+app.include_router(detail_objet_router.router, prefix="/detail_objets", tags=["DetailObjets"])
+
+#Utilisateur endpoints
+app.include_router(utilisateurs_router.router, prefix="/utilisateurs", tags=["Utilisateurs"])
+app.include_router(roles_router.router, prefix="/roles", tags=["Roles"])
+
+#Stock endpoints
+app.include_router(vignette_router.router, prefix="/vignettes", tags=["Vignettes"])
+app.include_router(poids_router.router, prefix="/poids", tags=["Poids"])
+app.include_router(objet_cond_router.router, prefix="/objet-cond", tags=["ObjetCond"])
 app.include_router(conditionnement_router.router, prefix="/conditionnement", tags=["Conditionnements"])
+#Programme fidelité endpoints
+app.include_router(programme_fidelite_router.router, prefix="/fidelite", tags=["Fidelite"])
+
+
 
 @app.get("/", response_class=HTMLResponse)
 def read_root(request: Request):
